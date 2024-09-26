a14724b5b2a88a1d9db897991782dccb00151809
Vid Kreča <34378985+VidKreca@users.noreply.github.com>
2024-09-26 09:10:23 +0200

    Forcing dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, DarkMode } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './Routes';
+import { theme } from './config';
 
 function App() {
   return (
-    <ChakraProvider>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <DarkMode>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </DarkMode>
     </ChakraProvider>
   );
 }
